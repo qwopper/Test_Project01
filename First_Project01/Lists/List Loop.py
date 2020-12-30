56c3343785d3d3134d06d2a50639f4236d375d85
qwopper <forsaken0915@gmail.com>
2020-12-29 21:28:30 -0700

    init

--- First_Project01/Lists/List Loop.py
+++ First_Project01/Lists/List Loop.py
@@ -1,2 +1,6 @@
+mari_favorite_food = ['MackyCheese', 'Piza', 'Chocolate', 'Poms']
+
+print(mari_favorite_food)  # Regular List
+
 for types in mari_favorite_food: # Prints every element in list: for (new list name here) in (main list): input desired action here
     print(f"{types} is mari's favorite food!")
