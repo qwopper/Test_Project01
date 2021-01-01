43cb5deefdba98112d7a7d701ba7ff2bb7c29ea2
qwopper <forsaken0915@gmail.com>
2021-01-01 00:06:01 -0700

    init

--- Valorant _Aim_Settings.py
+++ Valorant _Aim_Settings.py
@@ -4,7 +4,7 @@ print('Valorant Aim Sensitivity Settings according to DPI\n')
 DPI_400 = [.415, .375, .674, .600, 1, .628, .8, .82, .67, .974, .6, .6, .44, .471, .514, .5, .53, .54, .88, .69, 1, .8, .653, .76, .629, .58, .6, .55, .53, .7, .82, .58, .913, .447, .55, .624, .471, .6, .59, .785, .55, .619, .79, .817, .43, .59, 1, .459, .630, .515, .6, .58, .52, .6, .4, 1.12, .942, .434, .691, .650, .809, .58, .7, .597, .425, .69, .48, .45, .758, .47, .628, .408, .62, .5, .45, .563, .55, .97, .6, .6, .55, .55, .5, .8, .585, .390, .82, .6, .57, .404, .628, .534, .85, .535, .512]
 Average_Sensitivity_400 = str(float(mean(DPI_400)))
 print('400 DPI:')
-print(f'Collected {len(DPI_400)} diffrent settings from pros...')
+print(f'Collected {len(DPI_400)} different settings from pros...')
 print(f'The average sensitivty for 400 DPI is: {Average_Sensitivity_400}')
 print(f'The lowest number for 400 DPI is: {min(DPI_400)}')
 print(f'The highest number for 400 DPI is: {max(DPI_400)}')
