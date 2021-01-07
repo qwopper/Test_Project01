24645905c9d87bdf69300d521ec8fcd7c69f7af9
qwopper <forsaken0915@gmail.com>
2021-01-07 14:39:03 -0700

    init

--- OSSU CS/PY4E/Lesson 7/Strings/Parsing & Etracting.py
+++ OSSU CS/PY4E/Lesson 7/Strings/Parsing & Etracting.py
@@ -1,9 +1,9 @@
 data = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
-atpos = data.find('@')
-print(atpos)
+atpos = data.find('@') # Finding the firt '@' position in the data string
+print(atpos) # 21st position in string
 
-sppos = data.find(' ', atpos)
+sppos = data.find(' ', atpos) # Searching for the first space position after the 'atpos' position
 print(sppos)
 
-host = data[atpos+1 : sppos]
+host = data[atpos + 1 : sppos] # Adds 1 to include the 'u' in 'uct' since it starts from 21-31, it should be  22-23 to print 'uct.ac.za'
 print(host)
--- OSSU CS/PY4E/Lesson 7/Strings/Slice.py
+++ OSSU CS/PY4E/Lesson 7/Strings/Slice.py
@@ -1,4 +1,4 @@
-s = ' Monty Python'
+s = 'Monty Python'
 print(s[0:4])
 print(s[7])
 print(s[6:20]) # Stops at 11 even if it is requesting 20
